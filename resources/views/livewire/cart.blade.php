4c3ce85b9060541ceffbd09d7c5f50ff6bdebf4d
Eduardo Rahn <eduardo1590@gmail.com>
2021-07-13 16:14:55 -0400

    carro basico

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -9,7 +9,7 @@
                             <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Cantidad</th>
                             <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Precio</th>
                             <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Sub-Total</th>
-                            <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Actions</th>
+                            <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -17,20 +17,28 @@
                             <tr class="hover:bg-grey-lighter">
                                 <td class="py-4 px-6 border-b border-grey-light">{{ $item->name}}</td>
                                 <td class="py-4 px-6 border-b border-grey-light">{{ $item->qty }}</td>
-                                <td class="py-4 px-6 border-b border-grey-light">{{ $item->price }}</td>
-                                <td class="py-4 px-6 border-b border-grey-light">{{ $item->qty * $item->price }}</td>
+                                <td class="py-4 px-6 border-b border-grey-light">${{ $item->price }}</td>
+                                <td class="py-4 px-6 border-b border-grey-light">${{ $item->qty * $item->price }}</td>
                                 <td class="py-4 px-6 border-b border-grey-light">
-                                    <a wire:click="removeFromCart('{{ $id }}')" class="text-green-600 font-bold py-1 px-3 rounded text-xs bg-green hover:bg-green-dark cursor-pointer">Eliminar</a>
+                                    <a wire:click="removeFromCart('{{ $id }}')" class="text-blue-600 font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark cursor-pointer">Eliminar</a>
                                 </td>
                             </tr>
                         @endforeach
                     </tbody>
+                    <tfoot>
+                        <tr>
+                        <td class="py-4 px-6 border-b border-grey-light"></td>
+                        <td class="py-4 px-6 border-b border-grey-light"></td>
+                        <td class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">TOTAL:</td>
+                        <td class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">${{$total}}</td>
+                        <td class="py-4 px-6 border-b border-grey-light">
+                        </tr>
+                    </tfoot>
                 </table>
 
                 <div class="text-right w-full p-6">
-                <p>${{$total}}</p>
-                    <button wire:click="checkout()" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                        Checkout
+                    <button wire:click="checkout()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Confirmar
                     </button>
                 </div>
             @else
